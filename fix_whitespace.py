f9bfb3848390770dc0d13a97b75f9bdb73ccc9ed
Rekha chaudhary <chaudharyrekha8587@gmail.com>
2026-05-30 07:18:25 +0530

    style: remove trailing whitespace

---
+++ fix_whitespace.py
@@ -0,0 +1,8 @@
+import re
+for f in ['tests/test_register_validator.py', 'arnio/schema.py']:
+    with open(f, 'r') as file:
+        content = file.read()
+    cleaned = re.sub(r'[ \t]+\n', '\n', content)
+    with open(f, 'w') as file:
+        file.write(cleaned)
+print('Done!') 
